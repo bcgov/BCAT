e1bc4626e45b014de150458d32a0c23f67aeac23
Cory Atwater <cory@freshworks.io>
2023-07-12 14:41:30 -0400

    rename server folder to api, comment out migrations

--- api/src/migration/1675265105761-UpdateTableSchema.ts
+++ api/src/migration/1675265105761-UpdateTableSchema.ts
@@ -4,10 +4,10 @@ export class UpdateTableSchema1675265105761 implements MigrationInterface {
   name = 'UpdateTableSchema1675265105761';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "app_pbgp"."pbgp_application" ADD "deleted_at" TIMESTAMP`);
+    // await queryRunner.query(`ALTER TABLE "app_pbgp"."pbgp_application" ADD "deleted_at" TIMESTAMP`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "app_pbgp"."pbgp_application" DROP COLUMN "deleted_at"`);
+    // await queryRunner.query(`ALTER TABLE "app_pbgp"."pbgp_application" DROP COLUMN "deleted_at"`);
   }
 }
