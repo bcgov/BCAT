12c38f00a99934336d17f80d31f33905845f7053
Cory Atwater <cory@freshworks.io>
2023-07-27 13:17:55 -0400

    add audit columns

--- api/src/common/custom-base.entity.ts
+++ api/src/common/custom-base.entity.ts
@@ -13,12 +13,28 @@ export class CustomBaseEntity {
   @UpdateDateColumn({ name: 'APP_LAST_UPDATE_TIMESTAMP' })
   updatedAt: Date;
 
+  @Column({ name: 'CONCURRENCY_CONTROL_NUMBER', type: 'integer', default: 0 })
+  concurrencyControlNumber: number;
+
   // TO-DO: add relation, not quite sure what this guid is yet
   @Column({ name: 'APP_LAST_UPDATE_USER_GUID', type: 'varchar', length: 36 })
   lastUpdatedByUserGuid?: string;
 
   @Column({ name: 'APP_LAST_UPDATE_USER_ID', type: 'varchar', length: 30 })
   lastUpdatedByUserId?: string;
+
+  // audit columns
+  @CreateDateColumn({ name: 'DB_AUDIT_CREATE_TIMESTAMP' })
+  auditCreatedAt: string;
+
+  @UpdateDateColumn({ name: 'DB_AUDIT_LAST_UPDATE_TIMESTAMP' })
+  auditUpdatedAt: Date;
+
+  @Column({ name: 'DB_AUDIT_CREATE_USER_ID', type: 'varchar', length: 30 })
+  auditCreatedByUserId?: string;
+
+  @Column({ name: 'DB_AUDIT_LAST_UPDATE_USER_ID', type: 'varchar', length: 30 })
+  auditLastUpdatedByUserId?: string;
 
   // @VersionColumn()
   // version: number;
