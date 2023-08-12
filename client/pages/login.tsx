c03cecbae6696fa6e1ceee81dacf9085759f93f4
Alva Kostrova <Alva.Kostrova@ca.ey.com>
2023-08-11 19:44:43 -0700

    prettier

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -24,9 +24,7 @@ export default () => {
     <div className='h-full px-8 grid grid-cols-2 gap-20'>
       <div className='flex flex-col justify-center'>
         <h2 className='text-4xl'>B.C. Active Transportation</h2>
-        <p>
-          BCAT Description pending...
-        </p>
+        <p>BCAT Description pending...</p>
       </div>
       <div className='flex items-center'>
         <Button variant='primary' onClick={login}>
