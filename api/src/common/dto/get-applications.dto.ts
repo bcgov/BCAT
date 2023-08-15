d52f6735276caaf4506e7997082de48409367f5d
Alva Kostrova <Alva.Kostrova@ca.ey.com>
2023-08-15 16:37:34 -0700

    addressed comments

--- api/src/common/dto/get-applications.dto.ts
+++ api/src/common/dto/get-applications.dto.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { OrderByOptions, ApplicationSortOptions } from '../enums';
 import { PaginationDto } from './pagination.dto';
 
