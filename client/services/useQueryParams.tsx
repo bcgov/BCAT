21745a2d9c88f250969b499ae550e97dddf9c3a3
Cory Atwater <cory@freshworks.io>
2023-08-31 11:31:56 -0400

    GRANT-381 - fix unhandled exception cancel rendering route

--- client/services/useQueryParams.tsx
+++ client/services/useQueryParams.tsx
@@ -1,3 +1,7 @@
-export const SetQueryParams = (push: any, query: any, params: any): any => {
-  return push({ query: params }, undefined, { shallow: true });
+export const SetQueryParams = async (push: any, query: any, params: any): Promise<any> => {
+  try {
+    return await push({ query: params }, undefined, { shallow: true });
+  } catch {
+    return;
+  }
 };
