ff0233f6eaa8ffbd6be23cb613e7c07243bd336e
Alva Kostrova <Alva.Kostrova@ca.ey.com>
2023-09-07 09:34:31 -0700

    updated validation

--- api/src/applicationType/applicationType.entity.ts
+++ api/src/applicationType/applicationType.entity.ts
@@ -8,7 +8,7 @@ export class ApplicationType {
   @PrimaryGeneratedColumn({ name: 'APPLICATION_TYPE_ID' })
   id: number;
 
-  @Column({ name: 'NAME', type: 'varchar', length: 10, unique: true })
+  @Column({ name: 'NAME', type: 'varchar', length: 50, unique: true })
   name?: string;
 
   @OneToMany(() => Application, (application) => application.applicationType)
