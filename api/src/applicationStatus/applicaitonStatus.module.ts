53658acca145867ae62dcaf580da0fd254d5cee8
Alva Kostrova <Alva.Kostrova@ca.ey.com>
2023-09-07 09:39:54 -0700

    removed controllers for lookup tables

--- api/src/applicationStatus/applicaitonStatus.module.ts
+++ api/src/applicationStatus/applicaitonStatus.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ApplicationStatusController } from './applicationStatus.controller';
 import { ApplicationStatus } from './applicationStatus.entity';
 import { ApplicationStatusService } from './applicationStatus.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ApplicationStatus])],
   exports: [ApplicationStatusService],
-  controllers: [ApplicationStatusController],
   providers: [ApplicationStatusService],
 })
 export class ApplicationStatusModule {}
--- api/src/completionStatus/completionStatus.module.ts
+++ api/src/completionStatus/completionStatus.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompletionStatusController } from './completionStatus.controller';
 import { CompletionStatus } from './completionStatus.entity';
 import { CompletionStatusService } from './completionStatus.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CompletionStatus])],
   exports: [CompletionStatusService],
-  controllers: [CompletionStatusController],
   providers: [CompletionStatusService],
 })
 export class CompletionStatusModule {}
