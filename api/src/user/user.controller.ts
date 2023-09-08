0e7577f8be28a971b9c4f330bd990e497c394e99
Cory Atwater <99211385+coryatwater-FW@users.noreply.github.com>
2023-09-08 14:19:11 -0400

    GRANT-386_add-updated-by-team-mgmt-table (#62)

--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { UserAccessDto } from '../common/dto/user.dto';
 import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SUCCESS_RESPONSE, UserRoles } from '../common/constants';
-import { Roles } from '../common/decorator';
+import { GetUser, Roles } from '../common/decorator';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
@@ -19,8 +19,12 @@ export class UserController {
 
   @Patch('/:id/access')
   @Roles(UserRoles.ADMIN)
-  async updateUser(@Param('id') userId: number, @Body() body: UserAccessDto): Promise<any> {
-    await this.userService.updateUserAccess(userId, body);
+  async updateUser(
+    @Param('id') userId: number,
+    @Body() body: UserAccessDto,
+    @GetUser() user: User
+  ): Promise<any> {
+    await this.userService.updateUserAccess(userId, body, user);
     return SUCCESS_RESPONSE;
   }
 }
