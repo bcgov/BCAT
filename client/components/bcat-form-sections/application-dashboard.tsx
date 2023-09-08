0891272ab6d638ea638f85099cd51f7144be6347
Cory Atwater <cory@freshworks.io>
2023-09-08 10:36:23 -0400

    remove old input

--- client/components/bcat-form-sections/application-dashboard.tsx
+++ client/components/bcat-form-sections/application-dashboard.tsx
@@ -210,15 +210,6 @@ export const ApplicationDashboard: React.FC<any> = () => {
                 onChange={(e: any) => setState(p => ({ ...p, searchAssignedTo: e.target.value }))}
                 searchType={searchAssignedTo}
               />
-
-              <input
-                type='text'
-                className='bg-white rounded border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Total Estimated Cost'
-                onChange={e => setState(p => ({ ...p, searchTotalCost: e.target.value }))}
-                value={searchTotalCost}
-              />
-
               <div className='grid grid-cols-2 gap-1'>
                 <Button onClick={handleFilter} variant='primary' disabled={filterHasNoValues()}>
                   <FontAwesomeIcon icon={faFilter} className='h-4 mr-2' />
