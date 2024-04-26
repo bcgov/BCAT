51f936b8a21c54f736344b01fd58fd325983530f
Jerry Wang <jerry.wang@ca.ey.com>
2024-04-26 15:45:09 -0700

    fix description

--- api/src/application/application.view.entity.ts
+++ api/src/application/application.view.entity.ts
@@ -4,7 +4,7 @@ import { Application } from './application.entity';
 @ViewEntity({
   name: 'v_bcat_application',
   expression: `
-    create or replace view v_BCAT_APPLICATION as
+    create or replace view v_bcat_application as
     select 
       ba."SUBMISSION" -> 's1Container' ->> 's1FundingYear' as funding_year,
       *
