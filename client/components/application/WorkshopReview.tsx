5b8f850a5a041d4a0b797ffeec4ff5c359bc636e
Jerry Wang <jerry.wang@ca.ey.com>
2024-05-03 10:12:36 -0700

    back to disable

--- client/components/application/WorkshopReview.tsx
+++ client/components/application/WorkshopReview.tsx
@@ -81,7 +81,7 @@ export const WorkshopReview: React.FC<WorkshopReviewProps> = ({
                           variant='primary'
                           customClass='py-2 '
                           type='submit'
-                          disabled={!isValid || !values?.eligibilityScore}                          
+                          disabled={!isValid || !values?.eligibilityScore}
                         >
                           Save
                         </Button>
