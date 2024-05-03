b48a895fddc2b22f96622d0030dba9e8691fd654
Jerry Wang <jerry.wang@ca.ey.com>
2024-05-03 12:39:39 -0700

    add sort header basic fn; need more tweet

--- client/components/form/ApplicationTable.tsx
+++ client/components/form/ApplicationTable.tsx
@@ -1,34 +1,85 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 import { ApplicationTableProps } from '../../constants/interfaces';
 import { formatDate } from 'utils';
 
 type Props = { applications: ApplicationTableProps[] };
+enum Order {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+enum OrderBy {
+  CONFIRMATION_ID = 'confirmationId',
+  ASSIGNED_TO = 'assignedTo',
+  STATUS = 'status',
+  SUBMISSION_ID = 'submissionId',
+  UPDATED_AT = 'updatedAt',
+}
 
 const TableHeader: React.FC = () => {
-  const headers = [
-    'Confirmation ID',
-    'Funding Year',
-    'Applicant Name',
-    'Application Type',
-    'Project Title',
-    'Estimated Cost',
-    'Asks',
-    'Assigned to',
-    'Last Updated',
-    'Status',
+  const router = useRouter();
+  const { query } = router;
+  const currentSort: OrderBy | '' = (query.orderBy as OrderBy) || '';
+  const sortOrder: Order | undefined = (query.order as Order) || undefined;
+
+  const handleSort = (field: string) => {
+    // const order = (field === currentSort && sortOrder === Order.ASC) ? Order.DESC : Order.ASC;
+    let order: Order | undefined = Order.ASC;
+    if (field === currentSort && sortOrder === Order.ASC) {
+      order = Order.DESC;
+    } else if (field === currentSort && sortOrder === Order.DESC) {
+      order = undefined;
+    }
+    const newQuery = { ...query };
+    if (order) {
+      newQuery.orderBy = field;
+      newQuery.order = order;
+    } else {
+      delete newQuery.orderBy;
+      delete newQuery.order;
+    }
+    router.push({ query: newQuery });
+  };
+
+  const getSortIcon = (field: string) => {
+    if (field === currentSort) {
+      return sortOrder === Order.ASC ? '↑' : sortOrder === Order.DESC ? '↓' : '';
+    }
+    return '';
+  };
+
+  type Column = {
+    name: string;
+    field: string;
+  };
+
+  const columns: Column[] = [
+    { name: 'Confirmation ID', field: 'confirmationId' },
+    { name: 'Funding Year', field: 'fundingYear' },
+    { name: 'Applicant Name', field: 'applicantName' },
+    { name: 'Application Type', field: 'applicationType.name' },
+    { name: 'Project Title', field: 'projectTitle' },
+    { name: 'Estimated Cost', field: 'totalEstimatedCost' },
+    { name: 'Asks', field: 'asks' },
+    { name: 'Assigned to', field: 'assignedTo.displayName' },
+    { name: 'Last Updated', field: 'updatedAt' },
+    { name: 'Status', field: 'status.name' },
   ];
   const tdStyles =
-    'table-td table-header px-6 py-4 text-left text-sm font-strong border-b-2  border-bcYellowWarning';
+    'table-td table-header cursor-pointer px-6 py-4 text-left text-sm font-strong border-b-2  border-bcYellowWarning';
+
+  const headers = useMemo(() => {
+    return columns.map(({ name, field }) => (
+      <th key={field} className={tdStyles} onClick={() => handleSort(field)}>
+        {name} {getSortIcon(field)}
+      </th>
+    ));
+  }, [columns, currentSort, sortOrder]);
   return (
     <thead className='border-b bg-bcGrayInput table-header'>
-      <tr>
-        {headers &&
-          headers.map((title: string, index: number) => (
-            <th key={`th${index}`} className={tdStyles}>
-              {title}
-            </th>
-          ))}
-      </tr>
+      <tr>{headers}</tr>
     </thead>
   );
 };
